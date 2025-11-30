90c7b089f3a93b62c8b2ab3411aa0f7052378d49
kuzurgalievra <kuzurgalievra@teletribe.ru>
2025-11-30 19:51:06 +0400

    fix notification-service

---
+++ notification-service/app/Models/Message.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'destination',
+        'message',
+    ];
+
+    protected $casts = [
+        'user_id' => 'integer',
+        'type' => 'integer',
+    ];
+}
